c3c3fe9c1037281d0faee87788bbc76a45cec39b
AMEER KOTTA <53536783+AMEERKOTTA@users.noreply.github.com>
2023-01-12 10:16:57 +0530

    key_array_properties

---
+++ Section 2 - Numpy Primer/key_array_properties.py
@@ -0,0 +1,10 @@
+## defining a function to print the array properties.
+
+def array_key_properties(array):
+    
+    print(f" number of dimensions in an array (axes). :-- {array.ndim}")
+    print(f" size of the array in each dimensions. :-- {array.shape}")
+    print(f" total number of elements in the array. :-- {array.size}")
+    print(f" data type of the element in the array. :-- {array.dtype}")
+    
+    
